c3d483f529e3f6aea40692743da34107b0b7d0be
AikoCute <aikocute@icloud.com>
2022-07-29 11:02:03 +0700

    Update V0.0.3

--- AikoR/main.go
+++ AikoR/main.go
@@ -23,7 +23,7 @@ var (
 )
 
 var (
-	version  = "0.0.1"
+	version  = "0.0.3"
 	codename = "AikoR"
 	intro    = "Backend AikoR For Aiko"
 )
--- panel/panel.go
+++ panel/panel.go
@@ -144,7 +144,7 @@ func (p *Panel) loadCore(panelConfig *Config) *core.Instance {
 	if err != nil {
 		log.Panicf("failed to create instance: %s", err)
 	}
-	log.Printf("Aiko Core Version: %s", core.Version())
+	log.Printf("Xray Core Version: %s", core.Version())
 
 	return server
 }
