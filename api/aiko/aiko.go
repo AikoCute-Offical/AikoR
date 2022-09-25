cac3469c5c9f76de89f44d9350ce470fccc38a8c
AikoCute <aikocute@icloud.com>
2022-09-26 00:45:20 +0700

    Update aiko.go

--- api/aiko/aiko.go
+++ api/aiko/aiko.go
@@ -192,11 +192,11 @@ func (c *APIClient) GetUserList() (UserList *[]api.UserInfo, err error) {
 	var path string
 	switch c.NodeType {
 	case "V2ray":
-		path = "/api/v1/server/Vmess/users"
+		path = "/api/v1/server/Deepbwork/user"
 	case "Trojan":
-		path = "/api/v1/server/Trojan/users"
+		path = "/api/v1/server/TrojanTidalab/user"
 	case "Shadowsocks":
-		path = "/api/v1/server/Shadowsocks/users"
+		path = "/api/v1/server/ShadowsocksTidalab/user"
 	default:
 		return nil, fmt.Errorf("unsupported Node type: %s", c.NodeType)
 	}
@@ -239,11 +239,11 @@ func (c *APIClient) ReportUserTraffic(userTraffic *[]api.UserTraffic) error {
 	var path string
 	switch c.NodeType {
 	case "V2ray":
-		path = "/api/v1/server/Vmess/submit"
+		path = "/api/v1/server/Deepbwork/submit"
 	case "Trojan":
-		path = "/api/v1/server/Trojan/submit"
+		path = "/api/v1/server/TrojanTidalab/submit"
 	case "Shadowsocks":
-		path = "/api/v1/server/Shadowsocks/submit"
+		path = "/api/v1/server/ShadowsocksTidalab/submit"
 	}
 
 	data := make([]UserTraffic, len(*userTraffic))
