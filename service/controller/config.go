7ed92663978531c61371a0008202f4b5a8b57445
AikoCute <aikocute@icloud.com>
2022-10-02 23:58:33 +0700

    AutoSpeedLimitConfig

--- service/controller/config.go
+++ service/controller/config.go
@@ -1,23 +1,30 @@
 package controller
 
 type Config struct {
-	ListenIP             string            `mapstructure:"ListenIP"`
-	SendIP               string            `mapstructure:"SendIP"`
-	UpdatePeriodic       int               `mapstructure:"UpdatePeriodic"`
-	CertConfig           *CertConfig       `mapstructure:"CertConfig"`
-	EnableDNS            bool              `mapstructure:"EnableDNS"`
-	DNSType              string            `mapstructure:"DNSType"`
-	DisableUploadTraffic bool              `mapstructure:"DisableUploadTraffic"`
-	DisableGetRule       bool              `mapstructure:"DisableGetRule"`
-	EnableProxyProtocol  bool              `mapstructure:"EnableProxyProtocol"`
-	EnableFallback       bool              `mapstructure:"EnableFallback"`
-	DisableIVCheck       bool              `mapstructure:"DisableIVCheck"`
-	DisableSniffing      bool              `mapstructure:"DisableSniffing"`
-	FallBackConfigs      []*FallBackConfig `mapstructure:"FallBackConfigs"`
-	EnableIpRecorder     bool              `mapstructure:"EnableIpRecorder"`
-	IpRecorderConfig     *IpReportConfig   `mapstructure:"IpRecorderConfig"`
+	ListenIP             string                `mapstructure:"ListenIP"`
+	SendIP               string                `mapstructure:"SendIP"`
+	UpdatePeriodic       int                   `mapstructure:"UpdatePeriodic"`
+	CertConfig           *CertConfig           `mapstructure:"CertConfig"`
+	EnableDNS            bool                  `mapstructure:"EnableDNS"`
+	DNSType              string                `mapstructure:"DNSType"`
+	DisableUploadTraffic bool                  `mapstructure:"DisableUploadTraffic"`
+	DisableGetRule       bool                  `mapstructure:"DisableGetRule"`
+	EnableProxyProtocol  bool                  `mapstructure:"EnableProxyProtocol"`
+	EnableFallback       bool                  `mapstructure:"EnableFallback"`
+	DisableIVCheck       bool                  `mapstructure:"DisableIVCheck"`
+	DisableSniffing      bool                  `mapstructure:"DisableSniffing"`
+	AutoSpeedLimitConfig *AutoSpeedLimitConfig `mapstructure:"AutoSpeedLimitConfig"`
+	FallBackConfigs      []*FallBackConfig     `mapstructure:"FallBackConfigs"`
+	EnableIpRecorder     bool                  `mapstructure:"EnableIpRecorder"`
+	IpRecorderConfig     *IpReportConfig       `mapstructure:"IpRecorderConfig"`
 }
 
+type AutoSpeedLimitConfig struct {
+	Limit         int `mapstructure:"Limit"` // mbps
+	WarnTimes     int `mapstructure:"WarnTimes"`
+	LimitSpeed    int `mapstructure:"LimitSpeed"`    // mbps
+	LimitDuration int `mapstructure:"LimitDuration"` // minute
+}
 type CertConfig struct {
 	CertMode         string            `mapstructure:"CertMode"` // none, file, http, dns
 	RejectUnknownSni bool              `mapstructure:"RejectUnknownSni"`
