7079c138703aba8ca9f8d5520055eba3cec29ce3
AikoCute <aikocute@icloud.com>
2022-11-22 11:19:50 +0700

    Update limiter.go

--- common/limiter/limiter.go
+++ common/limiter/limiter.go
@@ -57,13 +57,9 @@ func (l *Limiter) AddInboundLimiter(tag string, nodeSpeedLimit uint64, userList 
 		l.g.redislimit = Redis.RedisLimit
 		l.g.redistimeout = Redis.RedisTimeout
 		l.g.expiry = Redis.Expiry
-		// Ping with Redis Server to check if it is alive
-		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(l.g.redistimeout)*time.Second)
-		defer cancel()
-		if err := l.r.Ping(ctx).Err(); err != nil {
-			return fmt.Errorf("redis ping failed: %s", err)
-		} else {
-			log.Printf("[%s] Redis ping success", tag)
+		// Ping redis
+		if err := l.r.Ping(context.Background()).Err(); err != nil {
+			log.Fatal(err)
 		}
 	}
 
