29d26bab01601745fe08598c7a13f3a9e22d4972
AikoCute <aikocute@icloud.com>
2022-11-28 17:09:44 +0700

    Update defaultConfig.go

--- panel/defaultConfig.go
+++ panel/defaultConfig.go
@@ -1,6 +1,8 @@
 package panel
 
-import "github.com/AikoCute-Offical/AikoR/service/controller"
+import (
+	"github.com/AikoCute-Offical/AikoR/service/controller"
+)
 
 func getDefaultLogConfig() *LogConfig {
 	return &LogConfig{
